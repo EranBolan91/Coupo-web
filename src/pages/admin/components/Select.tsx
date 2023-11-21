2a87cb51dd53c2280b0acf61a2c3a74fe611cd26
eran-bolandian <eranbolan91@gmail.com>
2023-11-21 21:37:36 +0200

    finished styling admin page

--- src/pages/admin/components/Select.tsx
+++ src/pages/admin/components/Select.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { CouponBrand } from "../../../types/Types";
 
 const people = [
   {
@@ -67,6 +68,7 @@ const people = [
 
 type Props = {
   selectTitle: string;
+  brands: CouponBrand[];
 };
 
 function classNames(...classes: string[]) {
@@ -74,7 +76,8 @@ function classNames(...classes: string[]) {
 }
 
 const Select = (props: Props) => {
-  const [selected, setSelected] = useState(people[3]);
+  const [selected, setSelected] = useState<CouponBrand>(props.brands[0]);
+  const { brands } = props;
 
   return (
     <Listbox value={selected} onChange={setSelected}>
@@ -86,12 +89,7 @@ const Select = (props: Props) => {
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
-                <img
-                  src={selected.avatar}
-                  alt=""
-                  className="h-5 w-5 flex-shrink-0 rounded-full"
-                />
-                <span className="ml-3 block truncate">{selected.name}</span>
+                <span className="ml-3 block truncate">{selected?.brand}</span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
                 <ChevronUpDownIcon
@@ -109,49 +107,53 @@ const Select = (props: Props) => {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
-                  <Listbox.Option
-                    key={person.id}
-                    className={({ active }) =>
-                      classNames(
-                        active ? "bg-indigo-600 text-white" : "text-gray-900",
-                        "relative cursor-default select-none py-2 pl-3 pr-9"
-                      )
-                    }
-                    value={person}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          <img
+                {brands &&
+                  brands.map((brand, index) => (
+                    <Listbox.Option
+                      key={index}
+                      className={({ active }) =>
+                        classNames(
+                          active ? "bg-indigo-600 text-white" : "text-gray-900",
+                          "relative cursor-default select-none py-2 pl-3 pr-9"
+                        )
+                      }
+                      value={brand}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center">
+                            {/* <img
                             src={person.avatar}
                             alt=""
                             className="h-5 w-5 flex-shrink-0 rounded-full"
-                          />
-                          <span
-                            className={classNames(
-                              selected ? "font-semibold" : "font-normal",
-                              "ml-3 block truncate"
-                            )}
-                          >
-                            {person.name}
-                          </span>
-                        </div>
+                          /> */}
+                            <span
+                              className={classNames(
+                                selected ? "font-semibold" : "font-normal",
+                                "ml-3 block truncate"
+                              )}
+                            >
+                              {brand.brand}
+                            </span>
+                          </div>
 
-                        {selected ? (
-                          <span
-                            className={classNames(
-                              active ? "text-white" : "text-indigo-600",
-                              "absolute inset-y-0 right-0 flex items-center pr-4"
-                            )}
-                          >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
+                          {selected ? (
+                            <span
+                              className={classNames(
+                                active ? "text-white" : "text-indigo-600",
+                                "absolute inset-y-0 right-0 flex items-center pr-4"
+                              )}
+                            >
+                              <CheckIcon
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))}
               </Listbox.Options>
             </Transition>
           </div>
